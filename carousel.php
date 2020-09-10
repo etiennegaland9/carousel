d35d707ec231409b51c28c1e3df1df1e410e3e77
Etienne GALAND <70742902+etiennegaland9@users.noreply.github.com>
2020-09-10 13:37:30 +0200

    Update carousel.php

--- carousel.php
+++ carousel.php
@@ -5,7 +5,105 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>carousel</title>
     <link rel="stylesheet" type="text/css" href="./Css/Im_Ui.css?refresh=<?php echo rand(2, 200) ?>" >
-    <link rel="stylesheet" href="./carousel_fondu.css" />
+    <style>
+      @import url("https://fonts.googleapis.com/css2?family=VT323&display=swap");
+* {
+  margin: 0;
+  padding: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+html {
+  font-size: 62.5%;
+  background-image: url(Fond.jpg);
+  background-attachment: fixed;
+}
+body {
+  width: 100%;
+  max-width: 1024px;
+  min-height: 870px;
+  margin: auto;
+}
+
+/* styles pour le carrousel : */
+.carousel {
+  position: relative;
+  width: 100%;
+  cursor: pointer;
+}
+.carousel .indicators {
+  width: 100%;
+  margin: 0 auto;
+  position: absolute;
+  bottom: 1rem;
+  z-index: 4;
+  text-align: center;
+  opacity: 0.5;
+}
+.carousel .indicators > span {
+  display: inline-block;
+  background-color: black;
+  border-radius: 50%;
+  border: 4px solid white;
+  width: 2rem;
+  height: 2rem;
+}
+.carousel .indicators > span.active {
+  background-color: white;
+  border: 4px solid black;
+}
+.carousel > .slides > * {
+  position: absolute;
+  top: 0;
+  left: 0;
+  opacity: 0;
+  transition: all 1s;
+  z-index: 0;
+  width: 100%;
+}
+.carousel .slides > *.active {
+  position: relative;
+  opacity: 1;
+  z-index: 2;
+}
+.carousel .slides > *.in {
+  z-index: 1;
+  opacity: 1;
+}
+.carousel .slides > *.out {
+  opacity: 1;
+  z-index: 2;
+}
+.carousel .arrow {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  color: white;
+  font-size: 1rem;
+  font-weight: 900;
+  background-color: black;
+  border-radius: 50%;
+  top: calc(50% - 1.5rem);
+  border: 4px solid white;
+  width: 3rem;
+  height: 3rem;
+  z-index: 2;
+  opacity: 0;
+  transition: opacity 1s;
+}
+.carousel:hover .arrow {
+  opacity: 1;
+}
+.carousel .arrow.left {
+  left: 1rem;
+}
+.carousel .arrow.right {
+  right: 1rem;
+}
+.carousel img {
+  max-width: 100%;
+}
+    <style/>
   </head>
   <body>
     
@@ -32,7 +130,74 @@
         <div class="indicators"></div>
       </div>
       
-    <script type="text/javascript" src="./main.js"></script>
+    <script>
+      function carouselHandler(carousel) {
+  //let carousel = document.querySelector(".carousel");
+  let slides = carousel.querySelectorAll(".slides > *");
+  let indicators;
+  let currentSlide = 0;
+
+  let intervalHandler;
+  carousel.addEventListener("mouseover", pause);
+  carousel.addEventListener("mouseout", autoplay);
+  carousel
+    .querySelector(".arrow.left")
+    .addEventListener("click", previousSlide);
+  carousel.querySelector(".arrow.right").addEventListener("click", nextSlide);
+
+  carousel.querySelector(".indicators").innerHTML = "<span></span>".repeat(
+    slides.length
+  );
+
+  indicators = carousel.querySelectorAll(".indicators > span");
+
+  indicators.forEach((element, index) => {
+    //function (element) { }
+    element.addEventListener("click", function () {
+      setSlide(index);
+    });
+  });
+
+  setSlide(currentSlide);
+  autoplay();
+
+  function autoplay() {
+    intervalHandler = setInterval(nextSlide, carousel.dataset.interval);
+  }
+
+  function pause() {
+    clearInterval(intervalHandler);
+  }
+  function nextSlide() {
+    index = (currentSlide + 1) % slides.length;
+    setSlide(index);
+  }
+  function previousSlide() {
+    index = (slides.length + currentSlide - 1) % slides.length;
+    setSlide(index);
+  }
+
+  function setSlide(index) {
+    
+    let activeSlide = carousel.querySelector(".slides > .active");
+
+    activeSlide.classList.remove("active");
+    indicators[currentSlide].classList.remove("active");
+
+    slides[index].classList.add("active");
+    indicators[index].classList.add("active");
+    currentSlide = index;
+  }
+
+  function getElementIndex(element) {
+    return [...element.parentNode.children].indexOf(element);
+  }
+}
+
+document.querySelectorAll(".carousel").forEach((carousel) => {
+  carouselHandler(carousel);
+});
+      </script>
   </body>
 </html>
 
